269200ede131a4c6c2e05f461e4e182047e9af00
jananathbanuka <jananath.banuka@e-bot7.com>
2022-06-11 00:56:01 +0530

    fixed unary operator issue

--- prerequisite.sh
+++ prerequisite.sh
@@ -5,7 +5,7 @@ echo "Hi"
 wget --no-verbose https://github.com/aquasecurity/trivy/releases/download/v0.18.3/trivy_0.18.3_Linux-64bit.deb
 sudo dpkg -i trivy_0.18.3_Linux-64bit.deb 2> /dev/null
 
-trivy image --format template --template '{{- $critical := 0 }}{{- $high := 0 }}{{- range . }}{{- range .Vulnerabilities }}{{- if  eq .Severity "CRITICAL" }}{{- $critical = add $critical 1 }}{{- end }}{{- if  eq .Severity "HIGH" }}{{- $high = add $high 1 }}{{- end }}{{- end }}{{- end }}Critical: {{ $critical }}, High: {{ $high }}' ${{ env.DOCKER_IMAGE_TO_SCAN }} | grep -i "Critical:" > scan_results 
+trivy image --format template --template '{{- $critical := 0 }}{{- $high := 0 }}{{- range . }}{{- range .Vulnerabilities }}{{- if  eq .Severity "CRITICAL" }}{{- $critical = add $critical 1 }}{{- end }}{{- if  eq .Severity "HIGH" }}{{- $high = add $high 1 }}{{- end }}{{- end }}{{- end }}Critical: {{ $critical }}, High: {{ $high }}' ${DOCKER_IMAGE_TO_SCAN} | grep -i "Critical:" > scan_results 
 
 content=$(cat scan_results)
 
@@ -17,7 +17,7 @@ slack_attachment_bar_color="#0CFE6B" # green
 summary_report_msg="No Vulnerabilities Found, Everything looks Good!"
 
 critical_emoji=":red_circle:"
-if [ "$critical2" == 0 ]; then
+if [[ $critical2 -eq 0 ]]; then
 critical_emoji=":white_check_mark:"
 else
 slack_attachment_bar_color="#FE360C" # red
@@ -26,9 +26,9 @@ fi
 
 high_emoji=":large_orange_diamond:"
 high_vulnerabilities_found="false"
-if [ "$high2" == 0 ]; then
+if [[ $high2 -eq 0 ]]; then
 high_emoji=":white_check_mark:"
-elif [ $critical2 == 0 ] && [ $high2  != 0 ]; then
+elif [[ $critical2 -eq 0 ]] && [[ $high2  -ne 0 ]]; then
 high_vulnerabilities_found="true"
 slack_attachment_bar_color="#FBB215" # orange\
 summary_report_msg="HIGH Vulnerabilities Found!, Have a Look!"
@@ -37,7 +37,7 @@ fi
 
 test_result_emoji=":boom:"
 critical_vulnerabilities_found="false"
-if [ $critical2 == 0 ] && [ $high2  == 0 ]; then
+if [[ $critical2 -eq 0 ]] && [[ $high2  -eq 0 ]]; then
 test_result_emoji=":eight_spoked_asterisk:"
 else
 critical_vulnerabilities_found="true"
